d0a18409130fb9e064349aec2ad3431641365c71
DESKTOP-TGFVELR\hanao <hanaonnaonao@gmail.com>
2022-12-17 16:03:37 +0800

    no message

--- src/components/CoverSFW.vue
+++ src/components/CoverSFW.vue
@@ -56,7 +56,13 @@ export default {
     },
 
     rjcode () {
-      return (`000000${this.workid}`).slice(-6)
+      if (this.workid >= 1000000)
+      {
+        return (`00000000${this.workid}`).slice(-8)
+      } else {
+        return (`000000${this.workid}`).slice(-6)
+      }
+      //return (`000000${this.workid}`).slice(-6)
     },
 
     imgClass () {
